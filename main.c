2897c30772c5ad21a26b57b7a16a75e9f70f6346
Maarten Joosten <mjoosten@f1r4s4.codam.nl>
2022-01-25 16:15:26 +0100

    Init

---
+++ main.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjoosten <mjoosten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 11:05:27 by mjoosten          #+#    #+#             */
+/*   Updated: 2022/01/25 16:14:48 by mjoosten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	ft_free_array(void **array);
+int		ft_gettime(void);
+
+void	*ft_thread(void *arg)
+{
+	(void)arg;
+	printf("%d Thread\n", ft_gettime());
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	pthread_t		**threads;
+	int				philo;
+	int				i;
+
+	if (argc < 2)
+		return (1);
+	philo = ft_atoi(argv[1]);
+	threads = malloc(sizeof(*threads) * (philo + 1));
+	threads[philo] = 0;
+	i = 0;
+	while (i < philo)
+	{
+		threads[i] = malloc(sizeof(pthread_t));
+		pthread_create(threads[i], 0, ft_thread, 0);
+		i++;
+	}
+	ft_free_array((void **)threads);
+	pthread_exit(0);
+}
+
+int	ft_atoi(char *str)
+{
+	int	result;
+
+	result = 0;
+	while (*str)
+		result = 10 * result + *str++ - '0';
+	return (result);
+}
+
+void	ft_free_array(void **array)
+{
+	void	**start;
+
+	if (!array)
+		return ;
+	start = array;
+	while (*array)
+		free(*array++);
+	free(start);
+}
+
+int	ft_gettime(void)
+{
+	static t_timeval	start;
+	t_timeval			time;
+
+	if (!start.tv_sec)
+		gettimeofday(&start, 0);
+	gettimeofday(&time, 0);
+	return ((time.tv_sec - start.tv_sec) * 1000
+		+ (time.tv_usec - start.tv_usec) / 1000);
+}
---
+++ philo.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjoosten <mjoosten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 11:06:55 by mjoosten          #+#    #+#             */
+/*   Updated: 2022/01/25 15:51:59 by mjoosten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+typedef struct timeval	t_timeval;
+
+int	ft_atoi(char *str);
+
+#endif
